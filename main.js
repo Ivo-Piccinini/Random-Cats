5732e3c3d2749e1516675e48e44d18d9652b314a
IvoPiccinini <ivopiccinini02@gmail.com>
2022-09-19 13:54:15 -0300

    Cambio del input type button por un button que recarga la foto y no la pagina

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const API = 'https://api.thecatapi.com/v1/images/search';
 
-fetch(API)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img')
-        img.src = data[0].url;
-    });
+async function reload() {
+    const res = await fetch(API);
+    const data = await res.json();
+
+    const img = document.querySelector('img')
+    img.src = data[0].url;
+}
+
+reload();
