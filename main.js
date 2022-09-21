bc350eb630fb6c61160a824a6eb4eb120a159053
IvoPiccinini <ivopiccinini02@gmail.com>
2022-09-21 10:08:40 -0300

    Implementacion de errores

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-const API = 'https://api.thecatapi.com/v1/images/search';
+const API_RANDOM = 'https://api.thecatapi.com/v1/images/search';
+const API_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_8iHi4RiIYufiGDZUcnDWfBuMYZOEXx827XVLDBik7aaasyqsDBw4Sn8HxKgzQDPM';
+const spanError = document.getElementById('error');
 
-async function reload() {
-    const res = await fetch(API);
+async function loadRandomCats() {
+    const res = await fetch(API_RANDOM);
     const data = await res.json();
 
-    const img = document.getElementById('rc')
-    img.src = data[0].url;
+    if(res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status;
+    } else {
+        const img = document.getElementById('rc')
+        img.src = data[0].url;
+    }
 }
 
-reload();
+async function loadFavouritesCats() {
+    const res = await fetch(API_FAVOURITES);
+    const data = await res.json();
+
+    if(res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    }
+}
+
+loadRandomCats();
+loadFavouritesCats();
