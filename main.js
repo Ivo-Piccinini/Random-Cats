63f163394c588788271e1cb3d3b45e49b024a764
IvoPiccinini <ivopiccinini02@gmail.com>
2022-09-21 13:11:05 -0300

    POST

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_RANDOM = 'https://api.thecatapi.com/v1/images/search';
-const API_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_8iHi4RiIYufiGDZUcnDWfBuMYZOEXx827XVLDBik7aaasyqsDBw4Sn8HxKgzQDPM';
+const API_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?api_key=live_4Ty4qaAc0dMSZes835g54DX2wIrQ5lJ1FYRp8lFJI7mtnsfa6kniMhyHZRacacAT';
 const spanError = document.getElementById('error');
 
 async function loadRandomCats() {
@@ -23,5 +23,23 @@ async function loadFavouritesCats() {
     }
 }
 
+async function saveFavouriteCats() {
+    const res = await fetch(API_FAVOURITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            image_id: '6g2'
+        }),
+    })
+    const data = await res.json();
+
+    if(res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    }
+}
+
 loadRandomCats();
 loadFavouritesCats();
+saveFavouriteCats();
