6a498e27d1bd0f3a0899423da1ad80b324940538
IvoPiccinini <ivopiccinini02@gmail.com>
2022-09-22 11:15:20 -0300

    seccion de favoritos html y js

--- main.js
+++ main.js
@@ -9,8 +9,12 @@ async function loadRandomCats() {
     if(res.status !== 200) {
         spanError.innerHTML = "Hubo un error: " + res.status;
     } else {
-        const img = document.getElementById('rc')
+        const img = document.getElementById('rc');
+        const btnFav = document.getElementById('btnFav');
+
         img.src = data[0].url;
+
+        btnFav.onclick = () => saveFavouriteCat(data[0].id);
     }
 }
 
@@ -20,20 +24,37 @@ async function loadFavouritesCats() {
 
     if(res.status !== 200) {
         spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    }else {
+        data.forEach(cat => {
+            const section = document.getElementById('favoritesCats');
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('remove');
+
+            img.width = 150;
+            img.src = cat.image.url;
+            btn.appendChild(btnText);
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article);
+        })
     }
 }
 
-async function saveFavouriteCats() {
+async function saveFavouriteCat(id) {
     const res = await fetch(API_FAVOURITES, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            image_id: '6g2'
+            image_id: id
         }),
     })
     const data = await res.json();
+
+    console.log(res);
 
     if(res.status !== 200) {
         spanError.innerHTML = "Hubo un error: " + res.status + data.message;
@@ -41,5 +62,5 @@ async function saveFavouriteCats() {
 }
 
 loadRandomCats();
+saveFavouriteCat();
 loadFavouritesCats();
-saveFavouriteCats();
